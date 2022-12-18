9672daf22f2aa9710816538d5593a033f5c54d4a
LedLight <ledlightsuper@yandex.ru>
2022-12-18 16:12:51 +0500

    minima 1.0.2

--- main.py
+++ main.py
@@ -5,25 +5,35 @@ import pygame
 pygame.init()
 pygame.display.set_caption('АвтоВАЗ_Гонки')
 screen = pygame.display.set_mode((1280, 720))
-bg = pygame.image.load('spirities/trassa.png')
+
+bg = pygame.image.load('spirities/trassa.png')  # фон
 
 
 class Car(pygame.sprite.Sprite):
-    image = pygame.image.load('spirities/2101.png')
-    image = pygame.transform.scale(image, (200, 100))
+    image = pygame.image.load('spirities/2109.png')
+    image = pygame.transform.scale(image, (200, 100))  # уменьшаем изображение
 
     def __init__(self, *group):
         super(Car, self).__init__(*group)
         self.image = Car.image
         self.rect = self.image.get_rect()
         self.rect.topleft = 1000, 210
+        self.get_configurations()
+
+    def get_configurations(self):
+        # with open('configurations.txt', encoding='utf8') as conf:  # все что ниже будем брать из txt. пока затычка
+        #     text = conf.readlines()
+
+        self.turn_speed = 4  # скорость поворота
+        self.max_speed = 100  # макс. скорость автомобиля
+        self.coef_scep = 5  # коэффициент сцепления (про запас)
 
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
-            self.rect.y += 3
+            self.rect.y += self.turn_speed
         if keys[pygame.K_a]:
-            self.rect.y -= 3
+            self.rect.y -= self.turn_speed  # скорость поворота
 
 
 clock = pygame.time.Clock()
